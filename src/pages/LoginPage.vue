660ce526c0864106d1fc2c7622b612825a111d51
anton <kosnamsss@gmail.com>
2024-08-07 11:39:41 +0300

    update authorization

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -2,14 +2,58 @@
   <q-layout>
     <q-page-container style="background-color: var(--q-primary); position: relative; overflow: hidden">
       <q-page>
-        <div style="display: flex;justify-content: center;align-items: center;z-index: 15;position: absolute;top: 50%;bottom: 50%;left: 50%;right: 50%;">
+        <div v-if="!this.requestSend" style="display: flex;justify-content: center;align-items: center;z-index: 15;position: absolute;top: 50%;bottom: 50%;left: 50%;right: 50%;">
           <div style="background-color: white; padding: 8px; width: 240px; height: 296px; border-radius: 4px; min-width: 240px; min-height: 296px">
-            <login-logo style="display: block; margin-left: auto; margin-right: auto;"/>
-            <div class="text-h6" style="text-align: center;margin-top: 8px;font-weight: 550">Войти в ULDESK</div>
+            <login-logo
+              style="display: block;margin-left: auto;margin-right: auto;"
+            />
+            <div
+              v-if="this.login"
+              class="text-h6"
+              style="text-align: center;margin-top: 8px;font-weight: 550"
+            >
+              Войти в ULDESK
+            </div>
+            <div
+              v-else-if="this.passwordRestore"
+              style="margin-top: 8px;"
+            >
+              <div style="display: flex; flex-direction: row;align-items: center">
+                <q-btn
+                  dense
+                  flat
+                  rounded
+                  @click="this.changeView('login')"
+                  style="margin-right: 3px;padding: 0"
+                  icon="arrow_back"
+                />
+                <div class="text-h6" style="text-align: center;font-weight: 550">
+                  Забыли пароль?
+                </div>
+              </div>
+            </div>
+            <div
+              v-else-if="this.emailRestore"
+              style="margin-top: 8px;"
+            >
+              <div style="display: flex; flex-direction: row;align-items: center">
+                <q-btn
+                  dense
+                  flat
+                  rounded
+                  @click="this.changeView('login')"
+                  style="margin-right: 9px;padding: 0"
+                  icon="arrow_back"
+                />
+                <div class="text-h6" style="text-align: center;font-weight: 550">
+                  Забыли почту?
+                </div>
+              </div>
+            </div>
             <form v-if="this.login" method="post" action="/perform_login" style="display: flex; flex-direction: column">
               <q-input
                 name="username"
-                style="margin-top: 16px"
+                style="margin-top: 16px;padding-bottom: 0"
                 v-model="username"
                 type="text"
                 label="Логин"
@@ -17,7 +61,7 @@
               />
               <q-input
                 name="password"
-                style="margin-top: 16px"
+                style="margin-top: 16px; padding-bottom: 0"
                 v-model="password"
                 :type="isPwd ? 'password' : 'text'"
                 label="Пароль"
@@ -31,7 +75,12 @@
                   />
                 </template>
               </q-input>
-              <a @click="this.changeView('passwordRestore')" style="color: var(--q-primary); margin-bottom: 15px; cursor: pointer">Забыл пароль?</a>
+              <a
+                @click="this.changeView('passwordRestore')"
+                style="color: var(--q-primary); margin-bottom: 15px; cursor: pointer;z-index: 1"
+              >
+                Забыл пароль?
+              </a>
               <q-btn
                 type="submit"
                 color="primary"
@@ -40,42 +89,54 @@
               </q-btn>
             </form>
             <form v-else-if="this.passwordRestore">
-              <div style="display: flex; flex-direction: row;align-items: center">
-                <q-btn
-                  style="margin-right: 20px;"
-                  dense
-                  flat
-                  rounded
-                  @click="this.changeView('login')"
-                  icon="arrow_back"
+              <div style="margin: 8px;text-align: center">Введите почту, с который связан ваш аккаунт</div>
+              <div>
+                <q-input
+                  name="email"
+                  style="padding: 0"
+                  v-model="this.email"
+                  type="text"
+                  label="Связанная почта"
                 />
-                <span style="justify-content: center">Забыли пароль?</span>
+                <a
+                  @click="this.changeView('emailRestore')"
+                  style="color: var(--q-primary);cursor: pointer;"
+                >
+                  Не помню почту
+                </a>
               </div>
-              <div style="margin: 8px;text-align: center">Введите почту с который связан ваш аккаунт</div>
-              <q-input
-                name="email"
-                style="margin-top: 16px"
-                v-model="this.email"
-                type="text"
-                label="Логин"
-                :rules="[val => (val && val.length > 0) || 'Обязательное поле']"
-              />
-              <a @click="this.changeView('emailRestore')" style="color: var(--q-primary); margin-bottom: 15px; cursor: pointer">Не помню почту</a>
-              <q-btn @click="this.resetPassword" style="width: 100%;margin-top: 14px;" type="submit" color="primary">Отправить запрос</q-btn>
+              <q-btn
+                @click="this.resetPassword"
+                style="width: 100%;margin-top: 45px;"
+                color="primary"
+              >
+                Отправить запрос
+              </q-btn>
             </form>
             <form v-else-if="this.emailRestore">
-              <span>
-                <q-btn
-                  dense
-                  rounded
-                  flat
-                  @click="this.changeView('login')"
-                  icon="arrow_back"
-                />
-                Забыли почту?
-              </span>
-              <q-btn type="submit" color="primary">Войти</q-btn>
+              <div style="text-align: center; padding: 8px">
+                Мы отправим запрос на восстановление пароля пользователю Admin
+              </div>
+              <div style="text-align: center;">Или напишите нам на почту:</div>
+              <div style="text-align: center;padding-bottom: 8px; color: gray;font-weight: 520">support@uldesk.com</div>
+              <q-btn @click="this.requestSend = true" style="width: 100%;margin-top: 51px" color="primary">Отправить запрос</q-btn>
             </form>
+          </div>
+        </div>
+        <div v-else style="display: flex;justify-content: center;align-items: center;z-index: 15;position: absolute;top: 50%;bottom: 50%;left: 50%;right: 50%;">
+          <div style="background-color: white; padding: 8px; width: 240px; height: 207px; border-radius: 4px; min-width: 240px; min-height: 207px;">
+            <login-logo
+              style="display: block;margin-left: auto;margin-right: auto;margin-top: 8px"
+            />
+            <div style="display: flex;flex-direction: row;align-items: center;margin-top: 16px; color: rgb(52, 199, 89);justify-content: center; font-weight: 520" class="text-h6">
+              <q-icon
+                style="color: rgba(52, 199, 89, 1)"
+                name="check"
+              />
+              Отправили запрос
+            </div>
+            <div style="text-align: center;margin-top: 16px">Мы отправили запрос на восстановление пароля</div>
+            <q-btn @click="this.closeNotify" style="color:white; background-color: rgb(52, 199, 89);width: 100%;margin-top: 16px">Отлично</q-btn>
           </div>
         </div>
         <div style="animation-delay: 0.45s;" class="backgroundImage">
@@ -152,7 +213,8 @@ export default {
     isPwd: true,
     login: true,
     passwordRestore: false,
-    emailRestore: false
+    emailRestore: false,
+    requestSend: false
   }),
 
   methods: {
@@ -179,7 +241,34 @@ export default {
     },
 
     resetPassword () {
+      const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+      const isValidEmail = emailRegex.test(this.email)
+      if (this.email.length === 0) {
+        this.$q.notify({
+          message: 'Поле почты не заполнено',
+          type: 'negative',
+          position: 'top-right',
+          actions: [{
+            icon: 'close', color: 'white', dense: true, handler: () => undefined
+          }]
+        })
+        return
+      }
+      if (!isValidEmail) {
+        this.$q.notify({
+          message: 'Почта указана не корректно',
+          type: 'negative',
+          position: 'top-right',
+          actions: [{
+            icon: 'close', color: 'white', dense: true, handler: () => undefined
+          }]
+        })
+        return
+      }
       axios.post('/api/v1/support/reset-password', { username: this.email })
+        .then(() => {
+          this.requestSend = true
+        })
         .catch(e => {
           this.$q.notify({
             message: e.message,
@@ -190,6 +279,11 @@ export default {
             }]
           })
         })
+    },
+
+    closeNotify () {
+      this.requestSend = false
+      this.changeView('login')
     }
     // login () {
     //   axios.post(`/login?username=${this.username}&password=${this.password}`)
